c56f8163225751ee3dad9e32d794f144860a7f1a
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-08-30 16:41:39 +0000

    Update guessing game project
    This is my implementation of the guessing game project up to the section
    on generating random numbers. The difference is I have broken things out
    into a number of helper functions.
    
    I also added a win condition, so if the player guesses in MAX_WINNING_TRIES,
    they are considered a winner.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,52 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    const MAX_WINNING_TRIES: usize = 3;
+    let number: String = String::from("5\n");
+    let tries = keep_guessing(&number);
+    if tries <= MAX_WINNING_TRIES {
+        println!("You win! It only took you {tries} tries to guess the correct number.");
+    } else {
+        println!("It took you {tries} tries to guess the correct number.");
+    }
+}
+
+fn keep_guessing(expected: &String) -> usize {
+    let mut tries: usize = 0;
+
+    loop {
+        tries += 1;
+
+        let guess = get_guess();
+
+        println!("You guessed: {guess}");
+        if check_guess(&guess, expected) {
+            break;
+        }
+    }
+
+    tries
+}
+
+fn get_guess() -> String {
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    guess
+}
+
+fn check_guess(guess: &String, expected: &String) -> bool {
+    if guess == expected {
+        return true
+    }
+
+    print!("Wrong! ");
+    false
 }
