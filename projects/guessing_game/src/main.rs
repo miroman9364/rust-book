bc56b9deb204a480549d6f3ad3d5bb91fdb5d46e
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-08-30 20:40:31 +0000

    Update to use numbers instead of strings - consume the input and parse it as a number - don't convert the secret to a string - added some hints during the guessing - increased the max winning tries to give player a better chance of winning (on average, it will take 7 tries to get the guess, so player needs to be better than average to "win")

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,15 +1,16 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    const MAX_WINNING_TRIES: usize = 3;
+    const MAX_WINNING_TRIES: usize = 5;
     const SHOULD_SPOIL_SECRET: bool = true;
 
     let number = get_number(SHOULD_SPOIL_SECRET);
 
-    let tries = keep_guessing(&number);
+    let tries = keep_guessing(number);
     if tries <= MAX_WINNING_TRIES {
         println!("You win! It only took you {tries} tries to guess the correct number.");
     } else {
@@ -17,8 +18,10 @@ fn main() {
     }
 }
 
-fn keep_guessing(expected: &String) -> usize {
+fn keep_guessing(expected: u32) -> usize {
     let mut tries: usize = 0;
+    let mut low_guess: u32 = 1;
+    let mut hi_guess: u32 = 100;
 
     loop {
         tries += 1;
@@ -26,7 +29,7 @@ fn keep_guessing(expected: &String) -> usize {
         let guess = get_guess();
 
         println!("You guessed: {guess}");
-        if check_guess(&guess, expected) {
+        if check_guess(guess, expected, &mut low_guess, &mut hi_guess) {
             break;
         }
     }
@@ -34,17 +37,16 @@ fn keep_guessing(expected: &String) -> usize {
     tries
 }
 
-fn get_number(spoil_the_secret: bool) -> String {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let number = format!("{secret_number}\n");
+fn get_number(spoil_the_secret: bool) -> u32 {
+    let number: u32 = rand::thread_rng().gen_range(1..=100);
     if spoil_the_secret {
-        print!("Psst! The number is: {number}");
+        println!("Psst! The number is: {number}");
     }
 
     number
 }
 
-fn get_guess() -> String {
+fn get_guess() -> u32 {
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -53,14 +55,29 @@ fn get_guess() -> String {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    guess
+    guess.trim().parse().expect("Please type a number!")
 }
 
-fn check_guess(guess: &String, expected: &String) -> bool {
-    if guess == expected {
-        return true;
+fn check_guess(guess: u32, expected: u32, low: &mut u32, hi: &mut u32) -> bool {
+    let is_match = match guess.cmp(&expected) {
+        Ordering::Less => {
+            println!("A cold wind blows over your left shoulder");
+            *low = guess;
+            false
+        }
+        Ordering::Greater => {
+            println!("A cold wind blows over your right shoulder");
+            *hi = guess;
+            false
+        }
+        Ordering::Equal => true,
+    };
+
+    if !is_match {
+        let rand_guess = rand::thread_rng().gen_range((*low + 1)..(*hi - 1));
+        let mid = (*low + *hi) / 2;
+        println!("The secret number is between {low} and {hi}, e.g. {rand_guess} or {mid}");
     }
 
-    print!("Wrong! ");
-    false
+    is_match
 }
