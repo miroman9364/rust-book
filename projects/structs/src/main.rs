bd47be2490f66ac861e7ef8cad5bbc9a0b457616
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-09-21 20:24:11 +0000

    Refactor for style - now that I know there can be multiple impl blocks, there seems to be no good reason to do this - not sure if pub is needed in the methods and associated functions

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,14 +1,3 @@
-
-impl Rectangle {
-    fn get_long_side(&self) -> u32 {
-        dbg!(self.width.max(self.height))
-    }
-
-    fn get_short_side(&self) -> u32 {
-        dbg!(self.width.min(self.height))
-    }
-}
-
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -16,8 +5,20 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    pub fn area(&self) -> u32 {
+    fn area(&self) -> u32 {
         dbg!(self.width * self.height)
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        dbg!(self.get_long_side() > other.get_long_side() && self.get_short_side() > other.get_short_side())
+    }
+
+    fn get_long_side(&self) -> u32 {
+        dbg!(self.width.max(self.height))
+    }
+
+    fn get_short_side(&self) -> u32 {
+        dbg!(self.width.min(self.height))
     }
 
     fn new(width: u32, height: u32) -> Rectangle {
@@ -38,10 +39,4 @@ fn main() {
         rect1.area(),
         rect1.can_hold(& Rectangle::new(rect1.height - 1, rect1.width - 1))
     );
-}
-
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        dbg!(self.get_long_side() > other.get_long_side() && self.get_short_side() > other.get_short_side())
-    }
 }
