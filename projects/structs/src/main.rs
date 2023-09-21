ca18a78acbb56d56d1c6cbcc21914d154628e528
miroman9364 <107484955+miroman9364@users.noreply.github.com>
2023-09-21 20:21:00 +0000

    Update implementation to use methods - the methods can be in one block or multiple - the methods can be anywhere in the create - functions inside an impl block that do not have self as the first parameter are not methods; they are known as associated functions and are invoked in a special way

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,7 +1,28 @@
+
+impl Rectangle {
+    fn get_long_side(&self) -> u32 {
+        dbg!(self.width.max(self.height))
+    }
+
+    fn get_short_side(&self) -> u32 {
+        dbg!(self.width.min(self.height))
+    }
+}
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    pub fn area(&self) -> u32 {
+        dbg!(self.width * self.height)
+    }
+
+    fn new(width: u32, height: u32) -> Rectangle {
+        dbg!(Rectangle { width, height })
+    }
 }
 
 fn main() {
@@ -12,12 +33,15 @@ fn main() {
     };
 
     println!(
-        "The area of the rectangle {:?} is {} square pixels.",
+        "The area of the rectangle {:?} is {} square pixels, and can hold {}.",
         rect1,
-        area(&rect1)
+        rect1.area(),
+        rect1.can_hold(& Rectangle::new(rect1.height - 1, rect1.width - 1))
     );
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    dbg!(rectangle.width * rectangle.height)
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        dbg!(self.get_long_side() > other.get_long_side() && self.get_short_side() > other.get_short_side())
+    }
 }
